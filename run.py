0f5027ac6c0e86d4335be03749e588551c03f6a8
TheBO$$ <40491968+thboss@users.noreply.github.com>
2020-06-21 15:27:04 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 from discord.ext.commands import has_permissions, CheckFailure
 import discord
+import json
+from json.decoder import JSONDecodeError
 from util import send_rcon_command
 
 TOKEN = 'xxxxxxxxxxxx'
@@ -41,7 +43,70 @@ async def rcon(ctx, *args):
     await ctx.send(embed=embed)
 
 
+@bot.command(brief='Force end specific live match')
+@has_permissions(administrator=True)
+async def endmatch(ctx, *args):
+    """force end specific live match"""
+    if len(args) < 1:
+        msg = 'Invalid usage <server number>'
+    else:
+        try:
+            server = SERVERS.split('--')[int(args[0]) - 1].split(':')
+        except (IndexError, TypeError, ValueError):
+            msg = 'Invalid usage <server number>'
+        else:
+            resp = send_rcon_command(server[0], int(server[1]), server[2], 'get5_endmatch')
+            if resp is None:
+                embed = discord.Embed(title=f'Server **{server[3]}** is down!')
+                await ctx.send(embed=embed)
+                return
+
+            if resp:
+                msg = f'Match canceled in server **{server[3]}** by admin **{ctx.author.display_name}**'
+            else:
+                msg = 'Currently, no live match in this server'
+
+    embed = discord.Embed(title=msg)
+    await ctx.send(embed=embed)
+
+
+@bot.command(brief='Live match status')
+async def live(ctx):
+    """send rcon command 'get5_status' to server and get response"""
+    isLive = False
+    servers = SERVERS.split('--')
+
+    for i in range(len(servers)):
+        server = servers[i].split(':')
+        resp = send_rcon_command(server[0], int(server[1]), server[2], 'get5_status')
+        if resp is None:
+            embed = discord.Embed(title=f'Server **{server[3]}** is down!')
+            await ctx.send(embed=embed)
+        else:
+            try:
+                resp = json.loads(resp)
+            except JSONDecodeError:
+                pass
+            else:
+                if resp['gamestate']:
+                    isLive = True
+                    msg = f'**Match ID :** {resp["matchid"]}\n' \
+                          f'**Match state :** {resp["gamestate_string"]}\n' \
+                          f'**Map :** {resp["maps"]["map0"]}\n' \
+                          f'**Score :**'
+
+                    embed = discord.Embed(title=server[3], description=msg)
+                    embed.add_field(name=f'{resp["team1"]["name"]}', value=f'{resp["team1"]["current_map_score"]}')
+                    embed.add_field(name=f'{resp["team2"]["name"]}', value=f'{resp["team2"]["current_map_score"]}')
+                    await ctx.send(embed=embed)
+
+    if not isLive:
+        embed = discord.Embed(title='No live matches for now')
+        await ctx.send(embed=embed)
+
+
 @rcon.error
+@endmatch.error
 async def rcon_error(ctx, error):
     if isinstance(error, CheckFailure):
         msg = "You don't have permission to use this command!"
