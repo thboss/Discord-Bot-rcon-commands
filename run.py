af21f2041ba3d6edca8dcca8b10b6e924d0ad4ad
TheBO$$ <40491968+thboss@users.noreply.github.com>
2020-09-02 23:16:37 +0300

    remove live command

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
 from discord.ext import commands
 from discord.ext.commands import has_permissions, CheckFailure
 import discord
-import json
-from json.decoder import JSONDecodeError
 from util import send_rcon_command
 
 TOKEN = 'xxxxxxxxxxxx'
-SERVERS = 'ip:port:rcon_password:server_name--ip:port:rcon_password:server_name'
+SERVERS = 'ip:port:rcon_password:server_name--ip:port:rcon_password:server_name--...etc'
 
 bot = commands.Bot(command_prefix='!')
 
@@ -41,41 +39,6 @@ async def rcon(ctx, *args):
 
     embed = discord.Embed(title=title, description=msg)
     await ctx.send(embed=embed)
-
-
-@bot.command(brief='Live match status')
-async def live(ctx):
-    """send rcon command 'get5_status' to server and get response"""
-    isLive = False
-    servers = SERVERS.split('--')
-
-    for i in range(len(servers)):
-        server = servers[i].split(':')
-        resp = send_rcon_command(server[0], int(server[1]), server[2], 'get5_status')
-        if resp is None:
-            embed = discord.Embed(title=f'Server **{server[3]}** is down!')
-            await ctx.send(embed=embed)
-        else:
-            try:
-                resp = json.loads(resp)
-            except JSONDecodeError:
-                pass
-            else:
-                if resp['gamestate']:
-                    isLive = True
-                    msg = f'**Match ID :** {resp["matchid"]}\n' \
-                          f'**Match state :** {resp["gamestate_string"]}\n' \
-                          f'**Map :** {resp["maps"]["map0"]}\n' \
-                          f'**Score :**'
-
-                    embed = discord.Embed(title=server[3], description=msg)
-                    embed.add_field(name=f'{resp["team1"]["name"]}', value=f'{resp["team1"]["current_map_score"]}')
-                    embed.add_field(name=f'{resp["team2"]["name"]}', value=f'{resp["team2"]["current_map_score"]}')
-                    await ctx.send(embed=embed)
-
-    if not isLive:
-        embed = discord.Embed(title='No live matches for now')
-        await ctx.send(embed=embed)
 
 
 @rcon.error
